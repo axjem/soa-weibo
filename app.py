82a92f658641b66664b623c252e932b475976749
AXJ <10inbinary@gmail.com>
2015-05-18 14:14:54 +0800

    Add utils and examples.

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, jsonify
+from utils import helloworld    # 这是个import我们写的模组的例子
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    helloworld.hello()  # 这是个调用我们写的函数的例子
+
     if request.method == 'POST':
         json_data = request.get_json()  # 读取POST请求中的JSON
         # 设计文件中定义传入格式: {"m_id": "104967351"}
