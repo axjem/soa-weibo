c136bd71d61a8dc19536a451bf2c4e20b44a4400
AXJ <10inbinary@gmail.com>
2015-06-03 08:18:50 +0800

    Format comments.

--- app.py
+++ app.py
@@ -29,35 +29,27 @@ def main():
     return jsonify(res)
 
 def make_retweet_statistics(m_id):
-    # 在db.retweet中找这条信息是否转发了其他信息
-    # 若是：则它不是祖先节点，在表项中找它的祖先节点
-        # 在db.retweet中找它的祖先微博original_mid
-        # 递归～ return make_retweet_statistics(original_mid)
-
-    # 若不是：则它是祖先节点
-        # u_ids = []
-        # 在db.retweet中找祖先微博的m_id为这个m_id的微博
-        # 得到一个集合
-        # 对于集合中的每条微博：
-            # 将u_id放入u_ids列表中
-
-        # 对于u_ids中的每一个u_id:
-            # 在user里面查找他的信息
-            # 使用几个计数器，比如如果性别为男性则male += 1, 有通过验证则verified += 1等
-        
-        # 每个计数器的值除以len(u_ids)即得出比例
     con = pymongo.MongoClient('127.0.0.1', 27017)
     db = con.weibodata
 
-    if db.retweet.find_one({'original_mid': m_id}) == None:
+    # 在db.retweet中找这条信息是否转发了其他信息
+    if db.retweet.find_one({'original_mid': m_id}) is None:
+    # 若是：则它不是祖先节点，在表项中找它的祖先节点
+
+        # 在db.retweet中找它的祖先微博original_mid
         original_mid = db.retweet.find_one({'retweet_mid': m_id})['original_mid']
+        # 递归
         return make_retweet_statistics(original_mid)
 
     else:
+    # 若不是：则它是祖先节点
         u_ids = []
+        # 在db.retweet中找祖先微博的m_id为这个m_id的微博
         retweet_weibo = db.retweet.find({'original_mid': m_id})
+        # 得到一个集合
 
         for item in retweet_weibo:
+            # 将u_id放入u_ids列表中
             u_ids.append(item['retweet_uid'])
 
         male_num = 0
@@ -68,7 +60,10 @@ def make_retweet_statistics(m_id):
         location_dist = {}
 
         for u_id in u_ids:
+            # 在user里面查找他的信息
             user = db.user.find_one({'u_id': u_id})
+
+            # 使用几个计数器，比如如果性别为男性则male += 1, 有通过验证则verified += 1等
             if user['gender'] == 'm':
                 male_num += 1
             if user['verified_type'] != '-1':   # 验证了的是不同号码挺有趣？
@@ -83,6 +78,7 @@ def make_retweet_statistics(m_id):
             else:
                 location_dist[location] = 1
 
+        # 每个计数器的值除以len(u_ids)即得出比例
         user_nums = len(u_ids)
         gender_ratio = male_num * 1.0 / user_nums
         verified_ratio = verified_num *  1.0 / user_nums
@@ -101,20 +97,30 @@ def make_retweet_statistics(m_id):
         return result
 
 def make_retweet_tree(m_id):
+    con = pymongo.MongoClient('127.0.0.1', 27017)
+    db = con.weibodata
+
+    result = []
+    child_weibo_data = {}
+
     # 在db.retweet中找这条信息是否转发了其他信息
+    if db.retweet.find_one({'original_mid': m_id}) == None:
     # 若是：则它不是祖先节点，在表项中找它的祖先节点
         # 在db.retweet中找它的祖先微博original_mid
-        # 递归～ return make_retweet_tree(original_mid)
+        original_mid = db.retweet.find_one({'retweet_mid': m_id})['original_mid']
+        # 递归
+        return make_retweet_tree(original_mid)
 
     # 若不是：则它是祖先节点
+    else:
         # 在db.retweet中找祖先微博的m_id为这个m_id的微博
+        retweet_weibo = db.retweet.find({'original_mid': m_id})
         # 得到一个集合
-        # 对于集合中的每条微博：
-            # 调用preprocess(它的m_id)
-            # 如：name, data = preprocess(它的m_id)
+
+        for item in retweet_weibo:
+            name, data = preprocess(item['retweet_mid'])
             # 得到它的用户名，及它的相关数据
-            # 存到child_weibo_data中
-            # 如：child_weibo_data[name] = data
+            child_weibo_data[name] = data
 
             # 例
             #~ child_weibo_data = {
@@ -125,17 +131,30 @@ def make_retweet_tree(m_id):
             #~ }
             # 表示显安和李雪转发了某条微博，马琳转发了显安，爽爷转发了李雪
 
-        # 对于child_weibo_data中的每一项：
-            # if not has_key("parent_name"):    （之类的，具体语法我想不起来）
-                # 是第一代转发微博，放入result中，
-                # m_id 为自己的m_id, parent 为祖先节点的m_id
-
-            # 否则：
+        for item in child_weibo_data:
+            if 'parent_name' not in child_weibo_data[item]:
+                # 是第一代转发微博，放入result中
+                result.append({
+                    'm_id': child_weibo_data[item]['m_id'], 
+                    'parent': m_id
+                })
+            else:
+                parent_name = child_weibo_data[item]['parent_name']
                 # 用它的parent_name去查child_weibo_data中的m_id，即父微博id
-                # 放到result中
+                if parent_name in child_weibo_data:
+                    parent_id = child_weibo_data[parent_name]['m_id']
+                    result.append({
+                        'm_id': child_weibo_data[item]['m_id'], 
+                        'parent': parent_id
+                    })
+                    # print parent_id
+                else:
+                    result.append({
+                        'm_id': child_weibo_data[item]['m_id'], 
+                        'parent': m_id
+                    })
 
-        # 返回result
-
+        return result
         # 例（续上方）：
         #~ result = [
         #~     {
@@ -155,51 +174,10 @@ def make_retweet_tree(m_id):
         #~         "parent": "3"
         #~     }
         #~ ]
-    con = pymongo.MongoClient('127.0.0.1', 27017)
-    db = con.weibodata
-
-    result = []
-    child_weibo_data = {}
-    if db.retweet.find_one({'original_mid': m_id}) == None:
-        original_mid = db.retweet.find_one({'retweet_mid': m_id})['original_mid']
-        return make_retweet_tree(original_mid)
-    else:
-        retweet_weibo = db.retweet.find({'original_mid': m_id})
-        for item in retweet_weibo:
-            name, data = preprocess(item['retweet_mid'])
-            child_weibo_data[name] = data
-            # print name, data
-        for item in child_weibo_data:
-            if 'parent_name' not in child_weibo_data[item]:
-                result.append({
-                    'm_id': child_weibo_data[item]['m_id'], 
-                    'parent': m_id
-                })
-            else:
-                parent_name = child_weibo_data[item]['parent_name']
-                if parent_name in child_weibo_data:
-                    parent_id = child_weibo_data[parent_name]['m_id']
-                    result.append({
-                        'm_id': child_weibo_data[item]['m_id'], 
-                        'parent': parent_id
-                    })
-                    # print parent_id
-                else:
-                    result.append({
-                        'm_id': child_weibo_data[item]['m_id'], 
-                        'parent': m_id
-                    })
-        return result
 
 def preprocess(m_id):
     """返回该微博的m_id, 用户名，及里面//@的第一个人的用户名。"""
 
-    # 查找db.weibo中它的text，得出parent_name
-    # 从u_id查找user中它的name
-
-    # 例：
-    # name = "爽爷"
-    # data = {"m_id": "4", "parent_name": "李雪"}
     con = pymongo.MongoClient('127.0.0.1', 27017)
     db = con.weibodata
     weibo = db.weibo.find_one({'mid': m_id})
@@ -212,13 +190,16 @@ def preprocess(m_id):
     pattern = re.compile(regExp)
     results = pattern.findall(tmp)
 
-    # print text
     if len(results) > 0:
         parent_name = results[0][3:-1]
         data = {'m_id': m_id, 'parent_name': parent_name}
     else:
         data = {'m_id': m_id}
+
     return name, data   # 二元组
+    # 例：
+    # name = "爽爷"
+    # data = {"m_id": "4", "parent_name": "李雪"}
 
 if __name__ == '__main__':
     reload(sys)
